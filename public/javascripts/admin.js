c3f3224d70d05ae411ae8ae12c9fc9286e205ff8
kylepotts <kyle@kptechblog.com>
2015-06-06 17:30:27 -0400

    add admin.js

---
+++ public/javascripts/admin.js
@@ -0,0 +1,18 @@
+$(document).ready(function(e){
+
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+		e.preventDefault();
+		var param = $(this).attr("href").replace("#","");
+		var concept = $(this).text();
+    fieldType=concept;
+		$('.search-panel span#search_concept').text(concept);
+		$('.input-group #search_param').val(param);
+	});
+  $("#search_button").click(function(){
+    var query = $("#search_query").val()
+    var filterType = $('.input-group #search_param').val()
+    console.log(fieldType);
+    console.log(query);
+
+  });
+});
