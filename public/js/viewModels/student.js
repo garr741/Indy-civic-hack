2e1a5e42a5d49694f775604374b04581a546fdf2
Ben Wencke <benwencke@gmail.com>
2015-06-06 15:04:57 -0400

    Added some form pages

--- public/js/viewModels/student.js
+++ public/js/viewModels/student.js
@@ -1,5 +1,15 @@
 function Student() {
 	var self = this;
-	self.firstName = ko.observable("Ben");
+	self.firstName = ko.observable("");
 	self.lastName = ko.observable("");
+	self.page = ko.observable(0);
+	self.onPageZero = ko.computed(function() {
+		return self.page() == 0;
+	}, this);
+	self.onPageOne = ko.computed(function() {
+		return self.page() == 1;
+	}, this);
+	self.toPageOne = function() {
+		self.page(1);
+	}
 }
